d81a8f8cd91230e12512aabad37af91036022f51
Grant Surlyn <grant@betabrand.com>
2019-09-28 19:49:02 -0700

    pawn capture logic; king move

--- src/chess/core.cljs
+++ src/chess/core.cljs
@@ -31,21 +31,35 @@
     (swap! game assoc :state :moving :active-piece
            {:color (square :color) :piece-type (square :piece-type) :row-index row-index :column-index column-index})))
 
-(defn is-legal-pawn-move [color starting-column ending-column starting-row ending-row]
-  (let [same-column-p (= ending-column starting-column)
-        initial-move-p (or (and (= color 'w) (= starting-row 6)) (= starting-row 1))
-        distance (if (= color 'w) (- starting-row ending-row) (- ending-row starting-row))]
-    (do (println " scp " same-column-p " imp " initial-move-p " d " distance)
-        (cond (= distance 1) same-column-p
-              (and (= distance 2) same-column-p) initial-move-p
-              :else false))))
+(defn get-color [row column]
+  ((nth (nth (@game :board) row) column) :color))
 
-(defn is-legal
+(defn is-legal-pawn-move-p [color starting-column ending-column starting-row ending-row]
+  (let [same-column-p (= ending-column starting-column)
+        x-distance (Math/abs (- starting-column ending-column))
+        initial-move-p (or (and (= color 'w) (= starting-row 6)) (= starting-row 1))
+        forward-y-distance (if (= color 'w) (- starting-row ending-row) (- ending-row starting-row))
+        landing-color (get-color ending-row ending-column)
+        different-color-p (and (some? landing-color ) (not= color landing-color))]
+    (cond (and (= forward-y-distance 1) (= x-distance 1) different-color-p) true
+          different-color-p false
+          (= forward-y-distance 1) same-column-p
+          (and (= forward-y-distance 2) same-column-p) initial-move-p
+          :else false)))
+
+(defn is-legal-king-move-p [color starting-column ending-column starting-row ending-row]
+  (let [column-distance (Math/abs (- starting-column ending-column))
+        row-distance (Math/abs (- starting-row ending-row))
+        one-square-move-p (and (<= column-distance 1) (<= row-distance 1))]
+    one-square-move-p))
+
+(defn is-legal-p
   "Take an active (moving) piece and a landing position,
   and return bool on whether the move is permitted."
   [{:keys [color piece-type row-index column-index]} landing-row landing-column]
-  (do (println "is-legal" color piece-type row-index column-index landing-row landing-column)
-      (cond (= piece-type 'p) (is-legal-pawn-move color column-index landing-column row-index landing-row)
+  (do (println "is-legal-p" color piece-type row-index column-index landing-row landing-column)
+      (cond (= piece-type 'p) (is-legal-pawn-move-p color column-index landing-column row-index landing-row)
+            (= piece-type 'k) (is-legal-king-move-p color column-index landing-column row-index landing-row)
             :else false)))
 
 (defn update-board! [active-piece landing-row landing-column]
@@ -62,7 +76,7 @@
   (let [landing-color (landing-square :color)
         active-piece (-> @game :active-piece)
         own-square-p (= (active-piece :color) landing-color)
-        legal-p (is-legal active-piece landing-row landing-column)]
+        legal-p (is-legal-p active-piece landing-row landing-column)]
     (do
       (println "land-piece" landing-color own-square-p legal-p landing-square landing-row landing-column)
       (cond
