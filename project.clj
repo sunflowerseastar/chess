bdae559c48c425ae8602f28b220b4085a4b849e3
Grant Surlyn <grant@sunflowerseastar.com>
2022-05-10 13:35:04 -0400

    add license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject chess "0.1.0-SNAPSHOT"
+(defproject chess "0.8.0"
   :description "Chess"
   :url "https://github.com/sunflowerseastar/chess"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU Affero General Public License v3"
+            :url "http://www.gnu.org/licenses/agpl-3.0.html"}
 
   :min-lein-version "2.7.1"
 
